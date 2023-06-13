3c253f2acb20dd723cc64f8cf4235e5984a6c24d
Martha Contreras <contrerasmartha2006@gmail.com>
2023-06-13 12:22:13 -0700

    Small Changes

--- CampFinder/ContentView.swift
+++ CampFinder/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Team 3!")
+            Text("Hi, Team 3!")
         }
         .padding()
     }

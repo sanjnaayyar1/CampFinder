f686f42fe2513e3500f805fb9fff044d6f4793a1
Sanjna Ayyar <ayyar.sanjna@gmail.com>
2023-06-13 09:54:56 -0500

    Initial Commit

---
+++ CampFinder/CampFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampFinderApp.swift
+//  CampFinder
+//
+//  Created by Sanjna Ayyar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CampFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

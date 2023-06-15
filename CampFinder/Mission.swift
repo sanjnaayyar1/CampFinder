f93aaaf63500c147e531f95295a7552bda0d857f
Martha Contreras <contrerasmartha2006@gmail.com>
2023-06-15 11:29:00 -0700

    applied background colors

--- CampFinder/Mission.swift
+++ CampFinder/Mission.swift
@@ -15,6 +15,9 @@ struct Mission: View {
             
             
         }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(red: 0.25882352941176473, green: 0.592156862745098, blue: 0.6274509803921569))
     }
 }
 
--- CampFinder/OurMission2.swift
+++ CampFinder/OurMission2.swift
@@ -20,6 +20,7 @@ struct OurMission2: View {
                 .padding()
             
             Text("Hello! We are part of the 2023 Kode with Klossy cohort (Go House F!)")
+                .multilineTextAlignment(.center)
 
             Image("Sanjna")
                 .resizable(resizingMode: .stretch)
@@ -30,6 +31,9 @@ struct OurMission2: View {
             }
             .alert("Hi, my name is Sanjna Ayyar! I'm a rising freshman at Rice University and while I am majoring in economics, I hope to continue my programming journey by minoring in CS!", isPresented: $presentAlert, actions:{})
             .padding()
+            .foregroundColor(.black)
+            .font(.title3)
+            .fontWeight(.semibold)
             
             Image("Anushree")
                 .resizable(resizingMode: .stretch)
@@ -40,6 +44,9 @@ struct OurMission2: View {
             }
             .alert("Hi, my name is Anushree Vijey! I am a senior in high school hoping to pursue software engineering and data science in the future!", isPresented: $presentSecondAlert, actions:{})
             .padding()
+            .foregroundColor(.black)
+            .font(.title3)
+            .fontWeight(.semibold)
             
             Image("Martha")
                 .resizable(resizingMode: .stretch)
@@ -49,7 +56,13 @@ struct OurMission2: View {
                 presentThirdAlert = true
             }
             .alert("Hi, my name is Martha Contreras and I am an upcoming senior in Hilmar High School.  I hope to obtain my masters in mechanical engineering and obtain a minor in computer science once I begin my college journey. ", isPresented: $presentThirdAlert, actions:{})
+            .foregroundColor(.black)
+            .font(.title3)
+            .fontWeight(.semibold)
         }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(red: 0.8980392156862745, green: 0.4980392156862745, blue: 0.5176470588235295))
     }
     
     struct OurMission2_Previews: PreviewProvider {

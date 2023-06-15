8a7edd457c4e3749c292f54bb8c6a2e55a7a5201
Martha Contreras <contrerasmartha2006@gmail.com>
2023-06-15 11:45:51 -0700

    Mission Page

--- CampFinder/Mission.swift
+++ CampFinder/Mission.swift
@@ -12,9 +12,22 @@ struct Mission: View {
     var body: some View {
         VStack{
             Text("Our Mission")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.black)
+                .padding()
+            
+            Text("CampFinder provides a platform that brings together computer science resources, mentorships, and educational opportunities specifically tailored for low-income communities.  With this app we aim to promote access and opportunities as well as create a supportive environment for individuals in underrepresented communities to explore and excel in computer science and engineering.")
+                .font(.system(size:45))
+                .fontWeight(.bold)
+                .foregroundColor(Color(red: 0.9568627450980393, green: 0.9176470588235294, blue: 0.9019607843137255))
+                .multilineTextAlignment(.center)
+                      
+                      
             
             
         }
+                      
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(red: 0.25882352941176473, green: 0.592156862745098, blue: 0.6274509803921569))
